c53be7408201358d229f9a9ab391cdcb01246bc0
Joe Lyons <josephjlyons90@gmail.com>
2021-04-29 14:52:21 -0400

    modify es-lint to get rid of warning for sanity

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
-export const getUsers = function() {
-    return axios.get("https://randomuser.me/api/?results=200&nat=us");
+// eslint-disable-next-line import/no-anonymous-default-export
+export default{
+  getUsers: function() {
+    return axios.get("https://randomuser.me/api/?results=100&nat=us");
+  },
   };
