6ac196714aa487caa4d7c2e17e2d7d117f2944c4
Joe Lyons <josephjlyons90@gmail.com>
2021-04-29 14:50:20 -0400

    start functionality and components added

--- src/components/MainContainer/MainContainer.js
+++ src/components/MainContainer/MainContainer.js
@@ -1,7 +1,53 @@
-import React from 'react'
+import React, { Component, useEffect, useState } from 'react'
+import SearchBar from '../SearchBar/Searchbar'
+import EmployeeTable from '../EmployeeTable/EmployeeTable'
+import API from '../../utils/API'
+import Wrapper from '../Wrapper/Wrapper';
 
-function Container(props) {
-  return <div className="container" style={props.style}>{props.children}</div>;
+class MainContainer extends Component {
+    state = {
+        employees: [],
+        search: ''
+    };
+
+    componentDidMount() {
+        API.getUsers()
+            .then((res) =>
+                this.setState({
+                    employees: res.data.results,
+                }))
+            .catch((err) => console.log(err))
+    };
+
+    handleInputChange = event => {
+        const value = event.target.value;
+        this.setState({ search: value });
+    };
+
+    handleFormSubmit = (event) => {
+        event.preventDefault();
+    };
+
+    render() {
+        return (
+            <Wrapper>
+            <SearchBar 
+            value={this.state.search}
+            handleInputChange={this.handleInputChange}
+            handleFormSubmit={this.handleFormSubmit}
+            />
+            <div className='container mt-4'>
+            
+            <EmployeeTable
+            state={this.state}
+            sortBy={this.sortBy}
+            filterEmployees={this.filterEmployees}
+            formatDate={this.formatDate}
+          />
+            </div>
+            </Wrapper>
+        )
+    }
 }
 
-export default Container;
+export default MainContainer;
