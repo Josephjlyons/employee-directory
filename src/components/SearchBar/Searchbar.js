7e6e49524ceb73271af9b7f1b0c79e06cabd9bcd
Joe Lyons <josephjlyons90@gmail.com>
2021-04-30 13:34:08 -0400

    search placeholder adjusted

--- src/components/SearchBar/Searchbar.js
+++ src/components/SearchBar/Searchbar.js
@@ -2,23 +2,23 @@ import React from "react";
 import "./SearchBar.css";
 
 const SearchBar = (props) => {
-  // Setting the component's initial state
+    return (
 
-  return (
-    <nav className="navbar navbar-light bg-light justify-content-center">
-      <form className="form-inline m-2" onSubmit={props.handleFormSubmit}>
-        <input
-          className="form-control"
-          value={props.value}
-          name="search"
-          onChange={props.handleInputChange}
-          type="search"
-          placeholder="Search"
-        />
-      </form>
-    </nav>
-    
-  );
+        <nav className="navbar  justify-content-center">
+            <h5>Search For an Employee</h5>
+            <form className="form-inline m-2" onSubmit={props.handleFormSubmit}>
+                <input
+                    className="form-control"
+                    value={props.value}
+                    name="search"
+                    onChange={props.handleInputChange}
+                    type="search"
+                    placeholder="Search for Employee"
+                />
+            </form>
+        </nav>
+
+    );
 };
 
 export default SearchBar;
